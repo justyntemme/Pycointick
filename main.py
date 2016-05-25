111c16796f08ae6ff262d2edfccf396e6b77ab2a
justyn <justyntemme@gmail.com>
2016-05-25 12:13:14 -0500

    fixed valueError, never put real numbers between

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ if not exists(CONFIG_PATH):
 	if not exists(CONFIG_DIR):
 		makedirs(CONFIG_DIR)
 	config['coinfetch'] = {}
-	config['coinfetch']['api'] = 'btce'
+	config['coinfetch']['api'] = 'ccc'
 	with open(CONFIG_PATH, 'w') as f:
 		config.write(f)
 else:
@@ -30,10 +30,12 @@ tickers = get_registered_tickers()
 tickers.sort()
 
 try:
-	print('%.8f' %get_ticker(default_api).get_rate("btc", "usd", "1"))
+	print('%.8f' %get_ticker("ccc").get_rate('btc', 'ltc', 1))
 except KeyError:
 	print('keyerror')
 	print(tickers)
 	exit(10)
 except ValueError:
 	print('valueerror')
+	print('%.8f' %get_ticker(default_api).get_rate('btc', 'ltc', '1'))
+	
