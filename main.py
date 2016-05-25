f8a6be74b9c5f290e632b56ce749f0addbb054ab
justyn <justyntemme@gmail.com>
2016-05-25 16:28:04 -0500

    Added Documentation

--- main.py
+++ main.py
@@ -6,15 +6,17 @@ from os.path import expanduser, join, exists
 from os import makedirs
 import os
 from time import sleep
+
 __title__ = 'cointick'
 __author__= 'Justyn Temme'
 
-
+# Set config file location
 CONFIG_DIR = join(expanduser('~'), '.cointick')
 CONFIG_PATH = join(CONFIG_DIR, 'config.ini')
 config= ConfigParser()
-
+# Check if config File exists or not
 if not exists(CONFIG_PATH):
+	# If file does not exist, create it and set default options
 	if not exists(CONFIG_DIR):
 		makedirs(CONFIG_DIR)
 	config['cointick'] = {}
@@ -25,16 +27,17 @@ if not exists(CONFIG_PATH):
 	with open(CONFIG_PATH, 'w') as f:
 		config.write(f)
 else:
-	config.read(CONFIG_PATH)
-
+	config.read(CONFIG_PATH) # If file does exist, Read it
+# Extract settings from config
 default_api = config['cointick']['api']
 default_currency = config['cointick']['curr']
 coins = str.split(config['cointick']['coins'],' ')
 refresh = int(config['cointick']['refresh'])
-
+# Load default plugins and tickers.
 load_default_plugins()
 tickers = get_registered_tickers()
 tickers.sort()
+# While the process is running, use the cfetch api to recieve currency based on settings.
 while (1):
 	os.system("clear")
 	for String in coins:
