dafa276fda47a03df74b5a1d489e42f99e80f1cb
justyn <justyntemme@gmail.com>
2016-05-25 11:57:58 -0500

    fixed keyError, plugins were not loaded yet

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from argparse import Action, ArgumentParser
+from configparser import ConfigParser
 from cfetch import __version__, get_ticker, get_registered_tickers
 from cfetch import load_default_plugins
 from os.path import expanduser, join, exists
-from configparser import ConfigParser
 from os import makedirs
 
 __title__ = 'cointick'
 __author__= 'Justyn Temme'
 
 
-CONFIG_DIR = join(expanduser('`'), '.coinfetch')
+CONFIG_DIR = join(expanduser('~'), '.coinfetch')
 CONFIG_PATH = join(CONFIG_DIR, 'config.ini')
 config= ConfigParser()
 
@@ -24,6 +24,8 @@ else:
 	config.read(CONFIG_PATH)
 
 default_api = config['coinfetch']['api']
+
+load_default_plugins()
 tickers = get_registered_tickers()
 tickers.sort()
 
@@ -31,8 +33,7 @@ try:
 	print('%.8f' %get_ticker(default_api).get_rate("btc", "usd", "1"))
 except KeyError:
 	print('keyerror')
-
-	list_sorted_apis()
+	print(tickers)
 	exit(10)
 except ValueError:
 	print('valueerror')
